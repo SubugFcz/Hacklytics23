8de3b343b0485a14261532342719145db610bb9c
rraharjo <122233338+rraharjo@users.noreply.github.com>
2023-02-11 17:55:22 -0500

    automate page redirect

--- hacklytics/hacklytics/urls.py
+++ hacklytics/hacklytics/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('stockify/', include("stockify.url"))
+    path('stockify/', include("stockify.url")),
+    re_path(r'.*', RedirectView.as_view(url = 'stockify/home/')),
 ]
