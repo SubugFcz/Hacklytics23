d94e95d958dbd10f0cf6806c8da1b929cc6d5d35
rraharjo <122233338+rraharjo@users.noreply.github.com>
2023-02-11 18:37:00 -0500

    transfer data to html

--- hacklytics/stockify/views.py
+++ hacklytics/stockify/views.py
@@ -8,7 +8,7 @@ import pandas as pd
 
 # Create your views here.
 def welcomePage(response):
-    file = os.path.join(settings.BASE_DIR, "stockify/AAPL.csv")
+    file = os.path.join(settings.BASE_DIR, "stockify/csvfiles/AAPL.csv")
     d = pd.read_csv(file, parse_dates=['Date'], dayfirst=True)
     df = pd.DataFrame(data=d, columns=['Date', 'Close'])
     closeList = df.values.tolist()
