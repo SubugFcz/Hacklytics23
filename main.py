34e63380f2545fff85b3a7cac24650a6445f0693
SubugFcz <ali.alamsyahdj@gmail.com>
2023-02-11 21:06:28 -0500

    try 10 csv

--- main.py
+++ main.py
@@ -3,20 +3,23 @@ import pandas as pd
 import os
 import numpy as np
 import functions as fn
+import matplotlib.pyplot as plt
 
 # List of 10 more .csv files
-files = ['file1.csv', 'file2.csv', 'file3.csv', 'file4.csv', 'file5.csv', 
-         'file6.csv', 'file7.csv', 'file8.csv', 'file9.csv']
+files = ['AAPL.csv', 'ADBE.csv', 'AMZN.csv', 'CSCO.csv', 'FTNT.csv', 
+         'MSFT.csv', 'MU.csv', 'NFLX.csv', 'NVDA.csv','STX.csv']
+names = ['Apple', 'Adobe', 'Amazon', 'Cisco', 'Ford Motor', 
+         'Microsoft', 'Micron', 'Netflix', 'Nvidia','Seagate Technology']
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    file = os.path.join(sys.path[0], 'AAPL.csv')
-    d = pd.read_csv(file, parse_dates=['Date'], dayfirst=True)
-    df = pd.DataFrame(data=d, columns=['Date', 'Close'])
-    df2 = pd.DataFrame(data=d, columns=['Date', 'Volume', 'Close'])
-    df1 = df.iloc[10200:10500]
-    df3 = df2.iloc[10200:10500]
-    #closeList = df1.values.tolist()
-    fn.plotOBV(df3)
-    fn.plotRSI(df1)
-    
+    for file, name in zip(files, names):
+        file = os.path.join(sys.path[0], file)
+        d = pd.read_csv(file, parse_dates=['Date'], dayfirst=True)
+        df = pd.DataFrame(data=d, columns=['Date', 'Close'])
+        df2 = pd.DataFrame(data=d, columns=['Date', 'Volume', 'Close'])
+        df1 = df.iloc[10200:10500]
+        df3 = df2.iloc[10200:10500]
+        #closeList = df1.values.tolist()
+        fn.plotOBV(df3, name)
+        fn.plotRSI(df1, name)
